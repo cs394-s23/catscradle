d445555745512c3799a6521ff757469a7f50b9dc
billyin0702 <billyin0702@outlook.com>
2023-03-30 19:24:01 -0500

    Readded index

--- src/Listing.jsx
+++ src/Listing.jsx
@@ -1,2 +1,16 @@
 import { useState } from 'react';
 import './Listing.css';
+
+const Listing = () => {
+    const [count, setCount] = useState(0);
+    
+    return (
+        <div className="Listing">
+            <header className="Listing-header">
+                <h1> Listing Details </h1>
+            </header>
+        </div>
+    );
+};
+
+export default Listing;
