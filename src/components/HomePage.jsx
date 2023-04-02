99d5e341ad654bc9829767d6f995abf180b7200f
erickwithack <lemaerick6@gmail.com>
2023-04-01 21:05:23 -0500

    modified background color

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import Post from "./Post";
 import "./Homepage.css";
 import logo from "../images/paw.jpeg";
+import { useNavigate } from "react-router-dom";
 
 import Apt from "../images/apt.jpg";
 import Rack from "../images/shoe-rack.jpg";
 
 const Homepage = () => {
+  let navigate = useNavigate();
+  const routeToListings = () => {
+    let path = "/listings";
+    navigate(path);
+  };
+
   return (
     <div className="root">
       <div className="homePage">
@@ -26,27 +33,32 @@ const Homepage = () => {
         </div>
 
         {/* buttons */}
-        <div className="buttons">
-          <button>All</button>
-          <button>Property</button>
-          <button>Furniture</button>
+        <div className="buttons-list">
+          <div className="buttons">
+            <button>All</button>
+            <button>Property</button>
+            <button>Furniture</button>
+          </div>
         </div>
 
         {/* Listings */}
         <div className="listings">
           <Post
+            onClick={routeToListings}
             postImage={Apt}
             postType={"Property"}
             Address={"1811 Sherman Abve 60201"}
             Caption={"1 bed 1 bath"}
           />
           <Post
+            onClick={routeToListings}
             postImage={Rack}
             postType={"Furniture"}
             Address={"1811 Sherman Abve 60201"}
             Caption={"IKEA shoe rack"}
           />
           <Post
+            onClick={routeToListings}
             postImage={Apt}
             postType={"Property"}
             Address={"1811 Sherman Abve 60201"}
