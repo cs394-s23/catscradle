d4a63b86325843efe9ae1119a9cb41a3ec5a9613
Rui Wen <ruiwen@ruis-air.ads.northwestern.edu>
2023-04-03 12:15:29 -0500

    display firestore data

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore'
+
+var firebaseConfig = {
+	apiKey: "AIzaSyCy9Y1VKbNv_4YpEPpW_R29ceaFJjms4AI",
+    authDomain: "catscradle-firebasetry5.firebaseapp.com",
+    databaseURL: "https://catscradle-firebasetry5-default-rtdb.firebaseio.com",
+    projectId: "catscradle-firebasetry5",
+    storageBucket: "catscradle-firebasetry5.appspot.com",
+    messagingSenderId: "480913266289",
+    appId: "1:480913266289:web:61aae8ee798c3f08536dd0",
+    measurementId: "G-KYXK8XYXFF"
+};
+
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+var db = firebaseApp.firestore();
+
+export default db;
--- src/components/Listing.jsx
+++ src/components/Listing.jsx
@@ -1,11 +1,67 @@
+// import "./Listing.css";
+// import Listing_images from "./Listing_images";
+// import Listing_details from "./Listing_Details";
+// import { useLocation } from "react-router-dom";
+
+// const Listing = (props) => {
+//   const location = useLocation();
+//   const document = location.state.props;
+
+//   return (
+//     <div className="Listing">
+//       <div className="Listing_top">
+//         <header className="Listing_header">
+//           <div className="Listing_header_content">
+//             <h1> {document.itemTitle} </h1>
+//             <a href="/" className="Return_home_button">
+//               {" "}
+//               <h4> Return to Home </h4>{" "}
+//             </a>
+//           </div>
+//         </header>
+//         <Listing_images document={document} />
+//       </div>
+//       <div className="Listing_details_in_general">
+//         <Listing_details document={document} />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Listing;
+
+
 import "./Listing.css";
 import Listing_images from "./Listing_images";
 import Listing_details from "./Listing_Details";
 import { useLocation } from "react-router-dom";
+import db from '../../firebase.js';
+import { useState } from 'react';
 
 const Listing = (props) => {
   const location = useLocation();
   const document = location.state.props;
+
+  const [info , setInfo] = useState([]);
+
+	// Start the fetch operation as soon as
+	// the page loads
+	window.addEventListener('load', () => {
+		Fetchdata();
+	});
+
+	// Fetch the required data using the get() method
+	const Fetchdata = ()=>{
+		db.collection("Properties").get().then((querySnapshot) => {
+			
+			// Loop through the data and store
+			// it in array to display
+			querySnapshot.forEach(element => {
+				var data = element.data();
+				setInfo(arr => [...arr , data]);
+			});
+		})
+	}
 
   return (
     <div className="Listing">
@@ -24,6 +80,14 @@ const Listing = (props) => {
       <div className="Listing_details_in_general">
         <Listing_details document={document} />
       </div>
+      {
+        info.map((data) => (
+          <div>
+            <p>{data.itemTitle}</p>
+            <p>{data.itemType}</p>
+          </div>
+        ))
+      }
     </div>
   );
 };
