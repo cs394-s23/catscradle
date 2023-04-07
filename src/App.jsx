44ccb9614c761a514ccc26dc2083fdffe32fb645
cracker_bitch <cracker_bitch@iMac.local>
2023-04-07 13:22:03 -0500

    created sign up and log in forms

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Listing from "./components/Listing";
 import Homepage from "./components/HomePage";
-
+import Signup from "./components/authentication/Signup";
+import Login from "./components/authentication/Login";
 
 function App() {
-
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Homepage/>} />
+          <Route path="/" element={<Homepage />} />
           <Route exact path="/listing" Component={Listing} />
+          <Route exact path="/signup" Component={Signup} />
+          <Route exact path="/login" Component={Login} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/authentication/Signup.jsx
@@ -0,0 +1,85 @@
+import logo from "./paw.jpeg";
+import "./Signup.css";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Signup = () => {
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setState((prevProps) => ({
+      ...prevProps,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(state);
+  };
+
+  return (
+    <div className="root">
+      <div className="sign-up">
+        {/* logo and app name */}
+        <div className="logo">
+          <p>
+            <span style={{ fontSize: "40px" }}>CatsCradle</span>
+            <span style={{ marginLeft: "5px" }}>
+              <img src={logo} />
+            </span>
+          </p>
+        </div>
+
+        {/* form */}
+        <h1>Sign Up</h1>
+
+        <div class="form-container">
+          <form onSubmit={handleSubmit}>
+            <div className="form-control">
+              <label>Email:</label>
+              <input
+                type="text"
+                name="email"
+                value={state.email}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="form-control">
+              <label>Password:</label>
+              <input
+                type="password"
+                name="password"
+                value={state.password}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="form-control">
+              <label>Repeat Password:</label>
+              <input
+                type="password"
+                name="password"
+                value={state.password}
+                onChange={handleInputChange}
+              />
+            </div>
+
+            <div className="submit-form">
+              <label></label>
+              <button type="submit">Sign Up</button>
+              <span className="login-caption">
+                already a member? <Link to="/login">log in</Link>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
